6dd91c1e67248ab77b9817ac2fef353f5f1a6772
Hans Vanrompay <hans.vanrompay@vito.be>
2024-04-11 15:53:32 +0200

    include BAP regression test

--- test_regression.py
+++ test_regression.py
@@ -1,5 +1,15 @@
+#%%
 import numpy as np
+import geopandas as gpd
+from openeo.processes import if_, is_nan
+
+
 from utils import extract_test_geometries, execute_and_assert
+
+from utils_BAP import (calculate_cloud_mask, calculate_cloud_coverage_score,
+                           calculate_date_score, calculate_distance_to_cloud_score,
+                           calculate_distance_to_cloud_score, aggregate_BAP_scores,
+                           create_rank_mask)
 
 
 def test_apply_kernel(auth_connection, tmp_path):
@@ -142,3 +152,69 @@ def test_mask_scl(auth_connection, tmp_path):
 
     # Excecute and assert
     execute_and_assert(cube, output_path, scenario_name)
+
+
+def test_BAP(auth_connection, tmp_path):
+
+    # Define scenario parameters
+    scenario_name = 'BAP'
+
+    # Set up output directory and path
+    output_path = tmp_path / f'output.nc'
+
+    # Parameters for data collection
+    collection_id = "SENTINEL2_L2A"
+    spatial_geometries = gpd.read_file('.\geofiles\BAP.geojson')
+    temporal_extent = ["2022-07-01", "2022-07-31"]
+    spatial_resolution = 20
+
+    # Fetch 100km x 100 km area from geojson
+    spatial_geometries = spatial_geometries.to_crs(epsg=4326)
+    area = eval(spatial_geometries.to_json())
+
+    # Get the spectral bands of interest
+    cube = auth_connection.load_collection(
+        collection_id,
+        temporal_extent = temporal_extent,
+        bands = ["B02", "B03","B04"],
+        max_cloud_cover=70
+    ).resample_spatial(spatial_resolution
+    ).filter_spatial(area)
+
+    # Get slc
+    scl = auth_connection.load_collection(
+        collection_id,
+        temporal_extent=temporal_extent,
+        bands=["SCL"],
+        max_cloud_cover=90
+    ).resample_spatial(spatial_resolution
+    ).filter_spatial(area
+    ).apply(lambda x: if_(is_nan(x), 0, x))
+
+
+    # Get cloud mask
+    cloud_mask =  calculate_cloud_mask(scl)
+
+    # Get scores
+    coverage_score = calculate_cloud_coverage_score(cloud_mask, area, scl)
+    date_score = calculate_date_score(scl)
+    dtc_score = calculate_distance_to_cloud_score(cloud_mask, spatial_resolution)
+
+    # Aggregate scores and create a mask
+    score = aggregate_BAP_scores(dtc_score, date_score, coverage_score)
+    score = score.mask(scl.band("SCL") == 0) #remove pixels that do not contain any data
+    rank_mask = create_rank_mask(score)
+
+    # Create composite
+    cube = cube.mask(rank_mask
+                    ).mask(cloud_mask
+                    ).aggregate_temporal_period("month","first")
+
+
+    # Excecute and assert
+    execute_and_assert(cube, output_path, scenario_name)
+
+
+
+
+    
--- utils.py
+++ utils.py
@@ -6,6 +6,8 @@ import json
 import openeo
 from typing import Union
 from pathlib import Path
+import numpy as np
+
 import pytest
 
 # Configure logging
@@ -99,20 +101,22 @@ def calculate_band_statistics(hypercube: xr.Dataset) -> dict:
 
     for band_name in band_names:
         band_data = hypercube[band_name]
-        mean_value = float(band_data.mean())
-        variance_value = float(band_data.var())
-        min_value = float(band_data.min())
-        max_value = float(band_data.max())
-        quantiles = band_data.quantile([0.25, 0.5, 0.75]).values
+        mean_value = np.round(float(band_data.mean()),2)
+        variance_value = np.round(float(band_data.var()),2)
+        min_value = np.round(float(band_data.min()),2)
+        max_value = np.round(float(band_data.max()),2)
+        quantile_25 = np.round(band_data.quantile([0.25]).values,2)
+        quantile_50 = np.round(band_data.quantile([0.5]).values,2)
+        quantile_75 = np.round(band_data.quantile([0.75]).values,2)
         
         statistics[band_name] = {
             'mean': mean_value,
             'variance': variance_value,
             'min': min_value,
             'max': max_value,
-            'quantile25': quantiles[0],
-            'quantile50': quantiles[1],
-            'quantile75': quantiles[2],
+            'quantile25': quantile_25[0],
+            'quantile50': quantile_50[0],
+            'quantile75': quantile_75[0],
         }
 
     return statistics
@@ -138,7 +142,7 @@ def execute_and_assert(cube: openeo.DataCube,
         RuntimeError: If there is an issue during execution, file saving, or assertion.
     """
     
-    cube.execute_batch(output_path,
+    cube.execute_batch(outputfile=output_path,
                         title=scenario_name,
                         description='benchmarking-creo',
                         job_options={'driver-memory': '1g'}
@@ -148,3 +152,67 @@ def execute_and_assert(cube: openeo.DataCube,
     output_dict = calculate_band_statistics(output_cube)
     groundtruth_dict = extract_reference_band_statistics(scenario_name)
     assert_band_statistics(output_dict, groundtruth_dict, tolerance)
+
+# functionality for updating the reference
+
+def update_json(json_name:str, scenario_name:str, new_statistics:dict):
+
+    # Convert NumPy arrays in new_statistics to Python lists
+    for key, value in new_statistics.items():
+        if isinstance(value, np.ndarray):
+            new_statistics[key] = value.tolist()
+
+    # Read the JSON file
+    with open(json_name, 'r') as file:
+        data = json.load(file)
+
+    # Find the scenario in the JSON data
+    found_scenario = False
+    for item in data:
+        if item['scenario_name'] == scenario_name:
+            # Update or add statistics for the scenario
+            item['reference_data'].update(new_statistics)
+            found_scenario = True
+            break
+
+    # If the scenario was not found, add it to the JSON data
+    if not found_scenario:
+        data.append({
+            'scenario_name': scenario_name,
+            'reference_data': new_statistics
+        })
+
+    # Write the updated JSON data back to the file
+    with open(json_name, 'w') as file:
+        json.dump(data, file, indent=4)
+
+def execute_and_update_reference(cube: openeo.DataCube, 
+                       output_path: Union[str, Path], 
+                       scenario_name: str,
+                       json_name:str
+                       ) -> None:
+    """
+    Execute the provided OpenEO cube, save the result to the output path, 
+    and assert its statistics against the reference data.
+
+    Parameters:
+        cube (openeo.datacube.DataCube): The OpenEO data cube to execute.
+        output_path (Union[str, Path]): The path where the output should be saved.
+        scenario_name (str): A name identifying the scenario for reference data.
+
+    Returns:
+        None
+
+    Raises:
+        RuntimeError: If there is an issue during execution, file saving, or assertion.
+    """
+    
+    cube.execute_batch(outputfile=output_path,
+                        title=scenario_name,
+                        description='benchmarking-creo',
+                        job_options={'driver-memory': '1g'}
+                        )
+
+    output_cube = xr.open_dataset(output_path)
+    output_dict = calculate_band_statistics(output_cube)
+    update_json(json_name, scenario_name, output_dict)
